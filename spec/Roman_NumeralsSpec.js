8504741f609a15f46a5d8f4e444a92b1a33ba9b8
Hung Ka Kin <hungkakin@Hungs-MacBook-Pro.local>
2018-10-05 22:57:56 +0800

    Add function to turn number 1 - 9 to roman numerals

--- spec/Roman_NumeralsSpec.js
+++ spec/Roman_NumeralsSpec.js
@@ -1,9 +1,24 @@
 describe('romanNumerals', function(){
-  it('return 1 as I', function() {
-    expect(romanNumerals(1)).toEqual('I')
-  });
 
-  it('return 3 as III', function() {
-    expect(romanNumerals(3)).toEqual('III')
-  })
+  describe('numbers 1 to 9', function() {
+    it('return 1 as I', function() {
+      expect(romanNumerals(1)).toEqual('I');
+    });
+
+    it('return 3 as III', function() {
+      expect(romanNumerals(3)).toEqual('III');
+    })
+
+    it('return 4 as IV', function() {
+      expect(romanNumerals(4)).toEqual('IV');
+    })
+
+    it('return 6 as VI', function() {
+      expect(romanNumerals(6)).toEqual('VI');
+    })
+
+    it('return 9 as IX', function() {
+      expect(romanNumerals(9)).toEqual('IX');
+    })
+  });
 });
--- src/Roman_Numerals.js
+++ src/Roman_Numerals.js
@@ -1,3 +1,13 @@
+const UNITS = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+
+
 function romanNumerals(number){
-  return 'I'.repeat(number)
+  return UNITS[number - 1]
+}
+
+
+function number_to_word(number){
+  if (number < 10) {
+    return romanNumerals(number)
+  }
 }
